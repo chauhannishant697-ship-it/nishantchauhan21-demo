3a20149c853c6a943ac972eed7dfba76fa77a516
Nishant Chauhan 21 <chauhannishant697@gmail.in>
2026-06-18 22:44:43 +0530

    Add New Project

--- Projects/TimedMathChallenge.py
+++ Projects/TimedMathChallenge.py
@@ -1,91 +1,44 @@
-"""
-                                           PROJECT NAME = TIMED MATH CHALLENGE 
-"""
-
 import random
 import time
 
-def add():
-    a= random.randint(1,99)
-    b= random.randint(1,99)
-    c= a+b
+MIN_VAL = 3
+MAX_VAL = 20
+OPERATORS = ["+", "-", "*"]
+TOTAL_Q = 10
 
-    i= int(input(f"{a} + {b}: "))
-    score = 0
-    if(i==c):
-        print("CORRECT....")
-        score += 1
-        print("YOUR SCORE: ",score)
+def math_problem():
+    i = random.randint(MIN_VAL, MAX_VAL)
+    j = random.randint(MIN_VAL, MAX_VAL)
+    op = random.choice(OPERATORS)
 
-    elif(i!=c):
-        print("INCORRECT... RIGHT ANSWER IS: ", c)
-        score= score
-        print('YOUR SCORE: ',score)
-        
-def sub():
-    a= random.randint(1,99)
-    b= random.randint(1,99)
-    c= a-b
+    expression = str(i) + " " + op + " " + str(j)
+    answer = eval(expression)
 
-    i= int(input(f"{a} - {b}: "))
-    score = 0
-    if(i==c):
-        print("CORRECT....")
-        score += 1
-        print("YOUR SCORE: ",score)
-
-    elif(i!=c):
-        print("INCORRECT... RIGHT ANSWER IS: ", c)
-        score= score
-        print('YOUR SCORE: ',score)
-
-choice=input("DO YOU WANT TO PLAY A GAME? (YES/NO): ").upper()
-
-count= 0
-start_time= time.perf_counter()
-
-while count < 5:
-    if choice== "YES":
-        print("LETS PLAY")
-
-    elif choice== "NO":
-        print("PLAY NEXT TIME.......")
-        break
-
-    else:
-        print("INVALID MOVE! PLEASE ENTER YES or NO")
-        break
-
-    add()
-    count += 1
-    
-while count < 5:
-    if choice== "YES":
-        print("LETS PLAY")
-
-    elif choice== "NO":
-        print("PLAY NEXT TIME.......")
-        break
-
-    else:
-        print("INVALID MOVE! PLEASE ENTER YES or NO")
-        break
+    return expression, answer
 
 
-    sub()
-    count +=1
-       
-end_time= time.perf_counter()
-Total_Time_Taken= end_time - start_time
-print(Total_Time_Taken,"Seconds")
+wrong = 0
 
-print("BEST OF LUCK FOR FUTURE")
+start_time = time.time()
 
+for z in range(TOTAL_Q):
 
+    expression, answer = math_problem()
 
+    while True:
 
+        guess = input(f"Problem #{z+1}: {expression} = ")
 
+        if guess == str(answer):
+            break
 
+        wrong += 1
+        print("Wrong! Try again.")
 
+end_time = time.time()
 
+total_time = round(end_time - start_time, 2)
 
+print("\n🎉 Game Over!")
+print(f"Total Time: {total_time} seconds")
+print(f"Wrong Attempts: {wrong}")
