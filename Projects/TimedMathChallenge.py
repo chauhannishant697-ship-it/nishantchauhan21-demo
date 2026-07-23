3a44b88618f19879f1c15865ea957f15cb6b7c7e
Nishant Chauhan 21 <chauhannishant697@gmail.in>
2026-07-23 23:51:28 +0530

    Add a new project

--- Projects/TimedMathChallenge.py
+++ Projects/TimedMathChallenge.py
@@ -7,38 +7,32 @@ OPERATORS = ["+", "-", "*"]
 TOTAL_Q = 10
 
 def math_problem():
-    i = random.randint(MIN_VAL, MAX_VAL)
-    j = random.randint(MIN_VAL, MAX_VAL)
-    op = random.choice(OPERATORS)
+    num1= random.randint(MIN_VAL, MAX_VAL)
+    num2= random.randint(MIN_VAL, MAX_VAL)
+    operator= random.choice(OPERATORS)
 
-    expression = str(i) + " " + op + " " + str(j)
+    expression = str(num1) + " " + operator + " " + str(num2)
     answer = eval(expression)
-
     return expression, answer
 
-
 wrong = 0
-
 start_time = time.time()
+for question in range(TOTAL_Q):
 
-for z in range(TOTAL_Q):
-
-    expression, answer = math_problem()
-
+    expression, answer = math_problem() 
     while True:
-
-        guess = input(f"Problem #{z+1}: {expression} = ")
-
-        if guess == str(answer):
+        solve= input(f"Problem #{question+1}: {expression} = ") 
+        if solve== str(answer): 
             break
 
-        wrong += 1
-        print("Wrong! Try again.")
+        else:
+            wrong += 1
+            print("Wrong! Try again.")
 
 end_time = time.time()
-
-total_time = round(end_time - start_time, 2)
+total_time = round(end_time - start_time, 3)
 
 print("\n🎉 Game Over!")
 print(f"Total Time: {total_time} seconds")
 print(f"Wrong Attempts: {wrong}")
+
