9d303a84d2a7aace7415ee812c0dfa86ef89659a
Nishant Chauhan 21 <chauhannishant697@gmail.in>
2026-07-23 23:09:18 +0530

    Add a new Project

--- Projects/RockPaperScissor.py
+++ Projects/RockPaperScissor.py
@@ -1,43 +1,69 @@
-"""
-                                PROJECT NAME = ROCK, PAPER, SCISSORS (GAME)
-"""
 
-
-#not completely looking well
+                                #PROJECT NAME = ROCK, PAPER, SCISSORS (GAME)
 
 import random
 
-game= ["ROCK", "PAPER", "SCISSOR"]
-i= (random.choice(game))
+def win():
+    print(f"Your Choice: {user_choice} and Opponent Choice: {rand_choice}")
+    print(f"You WIN")
+    print("Your Total Score: ", score)
 
-n= input("Let`s Play- ROCK, PAPER, SCISSOR: " ).upper()
+def lose():
+    print(f"Your Choice: {user_choice} and Opponent Choice: {rand_choice}")
+    print(f"You LOSE")
 
+def draw():
+    print(f"Your Choice: {user_choice} and Opponent Choice: {rand_choice}")
+    print(f"DRAW")  
+    print("Your Total Score: ", score)
+    
+name= input("ENTER YOUR NAME: ")
+game= ("ROCK", "PAPER", "SCISSOR")
+score= 0
 
 while True:
-    if(n==i):
-        print("DRAW:🤷‍♂️")
+    permission= input("For Play Press (P) and For Quit Press (Q): ").upper()
+    if permission== "P":
+        print("Let`s Play")
+
+        user_choice= input("Choose ROCK, PAPER, SCISSOR: ").upper()
+        rand_choice= random.choice(game)
+
+        if user_choice== rand_choice:
+            draw()
+
+        else:
+            if user_choice== "ROCK" and rand_choice== "SCISSOR":
+                score+= 5
+                win()
+
+            elif user_choice== "PAPER" and rand_choice== "ROCK":
+                score+= 5
+                win()
+
+            elif user_choice== "SCISSOR" and rand_choice== "PAPER":
+                score+= 5
+                win()
+
+            elif user_choice!= "ROCK" and user_choice!= "PAPER" and user_choice!= "SCISSOR" :
+                print("Invalid Request!")
+
+            else:
+                score-= 3
+                lose()
+                if score < 0:
+                    score=0
+                    print(f"YOUR SCORE: {score}")
+
+                else:
+                    print(f"Your Total Score: {score}")
+
+    elif permission== "Q":
+        print("THANKS FOR PLAYING")
+        print("Your Total Score: ", score)
         break
-    elif(n!=i):
-        if(n== "PAPER" and i== "ROCK"):      #i=ROCK
-            print("CORRECT.....YOU WIN 💖")
-            break
-        elif(n=="SCISSOR" and i== "ROCK"):
-            print("INCORRECT.....YOU LOSE 😭")
-            break
 
-        elif(n== "SCISSOR" and i== "PAPER"):  #i= PAPER
-            print("CORRECT.....YOU WIN 💖")
-            break
-        elif(n=="ROCK" and i== "PAPER"):
-            print("INCORRECT.....YOU LOSE 😭")
-            break
-        
+    else:
+        print("INVALID REQUEST!")
 
-        if(n== "ROCK" and i== "SCISSOR"):       #i= SCISSOR
-            print("CORRECT.....YOU WIN 💖")
-            break
-        elif(n=="PAPER" and i== "SCISSOR"):
-            print("INCORRECT.....YOU LOSE 😭")
-            break
 
-print("💖....GAME OVER....💖")
